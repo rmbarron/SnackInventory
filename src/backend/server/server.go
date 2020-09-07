0dba35c2e10a0adf29035650b54fb2fe273deaaa
Robert Barron <rmbarron3@gmail.com>
2020-09-07 15:03:48 -0700

    Add connector to MariaDB

--- src/backend/server/server.go
+++ src/backend/server/server.go
@@ -13,30 +13,63 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 // Package main provides a gRPC service to interact with SnackInventory storage.
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
+	"github.com/rmbarron/SnackInventory/src/backend/server/connector"
 	sipb "github.com/rmbarron/SnackInventory/src/proto/snackinventory"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-var portFlag = flag.Int("port", 10000, "Port for SnackInventory to listen on.")
+var (
+	portFlag        = flag.Int("port", 10000, "Port for SnackInventory to listen on.")
+	storageImplFlag = flag.String(
+		"storage_architecture", "mysql",
+		`Architecture to use for backing storage. Valid values include:
+		 - mysql`)
 
-type snackInventoryServer struct{}
+	// Flags for connector.SQLImpl.
+	sqlUserFlag = flag.String("sql_user", "", "Username for connecting to MySQL.")
+	sqlAddrFlag = flag.String(
+		"sql_address", "", "host:port address for connecting to MySQL.")
+	sqlDBNameFlag = flag.String(
+		"sql_database", "SnackInventory", "MySQL database name to connect to.")
+)
+
+// Interface for connecting to backing storage.
+type dbConnector interface {
+	ListSnacks(ctx context.Context) ([]*sipb.Snack, error)
+}
+
+type snackInventoryServer struct {
+	c dbConnector
+}
 
 func (s *snackInventoryServer) CreateSnack(ctx context.Context, req *sipb.CreateSnackRequest) (*sipb.CreateSnackResponse, error) {
 	return &sipb.CreateSnackResponse{}, nil
 }
 
 func (s *snackInventoryServer) ListSnacks(ctx context.Context, req *sipb.ListSnacksRequest) (*sipb.ListSnacksResponse, error) {
-	return &sipb.ListSnacksResponse{}, nil
+	snacks, err := s.c.ListSnacks(ctx)
+	if err != nil {
+		// TODO: Translate storage errors to corresponding canonical code.
+		return nil, status.Errorf(codes.Internal, "could not list snacks: %v", err)
+	}
+	return &sipb.ListSnacksResponse{
+		Snacks: snacks,
+	}, nil
 }
 
 func (s *snackInventoryServer) DeleteSnack(ctx context.Context, req *sipb.DeleteSnackRequest) (*sipb.DeleteSnackResponse, error) {
@@ -45,6 +78,38 @@ func (s *snackInventoryServer) DeleteSnack(ctx context.Context, req *sipb.Delete
 
 func main() {
 	flag.Parse()
+
+	var c dbConnector
+	var err error
+	switch si := *storageImplFlag; si {
+	case "mysql":
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		pwd := scanner.Text()
+
+		if *sqlUserFlag == "" {
+			log.Fatal("--sql_user is required.")
+		}
+
+		if *sqlAddrFlag == "" {
+			log.Fatal("--sql_address is required.")
+		}
+
+		if *sqlDBNameFlag == "" {
+			log.Fatal("--sql_database is required.")
+		}
+
+		c, err = connector.NewSQLImpl(context.Background(), *sqlUserFlag, pwd, *sqlAddrFlag, *sqlDBNameFlag)
+		if err != nil {
+			log.Fatalf("could not connect to SQL: %v", err)
+		}
+	default:
+		log.Fatal("unsupported storage implementation requested.")
+	}
+
+	si := &snackInventoryServer{
+		c: c,
+	}
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *portFlag))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -52,7 +117,7 @@ func main() {
 
 	// TODO: Serve with TLS.
 	grpcServer := grpc.NewServer()
-	svc := sipb.NewSnackInventoryService(&snackInventoryServer{})
+	svc := sipb.NewSnackInventoryService(si)
 	sipb.RegisterSnackInventoryService(grpcServer, svc)
 	grpcServer.Serve(lis)
 }
